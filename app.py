3f7ba2171f28536bd133d01d39caa517e232d092
Caroline Kiorpes <cekiorpes@gmail.com>
2022-04-03 17:29:47 -0400

    Renamed model.py to models.py; working on printing data from database in app

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from flask import (
     jsonify,
     request,
     redirect)
-from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 from config import username
 from config import password
 
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{username}:{password}@localhost:5432/Project3"
 
 # Remove tracking modifications
@@ -20,40 +20,48 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-from .model import Ukraine
-
-# connection = psycopg2.connect(database="Project3", user=username, password=password, host="localhost", port="5432")
-# cursor = connection.cursor()
+from models import Ukraine
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # return render_template("index.html")
+    return (
+        "Welcome!<br/>"
+        "Available routes:<br/>"
+        f"/api/data"
+    )
 
-@app.route("/api/data", methods = ['POST', 'GET'])
-def data(date, event, actor):
-    if request.method == 'POST':
-        events1 = db.session.query(Ukraine).filter_by(EVENT_DATE = date).all()
-        events2 = db.session.query(Ukraine).filter_by(EVENT_TYPE = event).all()
-        events3 = db.session.query(Ukraine).filter_by(ACTOR1 = actor).all()
-    # elif request.method == 'GET':
-    #     conflicts = Ukraine.query.all()
-    #     results = [
-    #         {
-                # "EVENT_ID_CNTY": conflict.EVENT_ID_CNTY,
-	            # "EVENT_ID_NO_CNTY": conflict.EVENT_ID_NO_CNTY
-                # "EVENT_DATE": db.Column(db.String())
-                # "EVENT_TYPE": db.Column(db.String())
-                # "SUB_EVENT_TYPE": db.Column(db.String())
-                # "ACTOR1":  db.Column(db.String())
-                # "LOCATION": db.Column(db.String())
-                # "LATITUDE": db.Column(db.Float())
-                # "LONGITUDE": db.Column(db.Float())
-                # "SOURCE": db.Column(db.String)
-                # "NOTES": db.Column(db.String)
-                # "FATALITIES": conflict.FATALITIES
-    #         } for conflict in conflicts
-    #     ]
+
+@app.route("/api/data", methods=['POST', 'GET'])
+def data():
+    # if request.method == 'POST':
+    #     events1 = db.session.query(Ukraine).filter_by(EVENT_DATE=date).all()
+    #     events2 = db.session.query(Ukraine).filter_by(EVENT_TYPE=event).all()
+    #     events3 = db.session.query(Ukraine).filter_by(ACTOR1=actor).all()
+    # elif:
+    conflicts = db.session.query(Ukraine.EVENT_ID_CNTY, Ukraine.EVENT_ID_NO_CNTY, Ukraine.EVENT_DATE, Ukraine.EVENT_TYPE, Ukraine.SUB_EVENT_TYPE, Ukraine.ACTOR1, Ukraine.LOCATION, Ukraine.LATITUDE, Ukraine.LONGITUDE, Ukraine.SOURCE, Ukraine.NOTES, Ukraine.FATALITIES).all()
+
+    results = [
+        {
+            "EVENT_ID_CNTY": conflict.EVENT_ID_CNTY,
+            "EVENT_ID_NO_CNTY": conflict.EVENT_ID_NO_CNTY,
+            "EVENT_DATE": conflict.EVENT_DATE,
+            "EVENT_TYPE": conflict.EVENT_TYPE,
+            "SUB_EVENT_TYPE": conflict.SUB_EVENT_TYPE,
+            "ACTOR1":  conflict.ACTOR1,
+            "LOCATION": conflict.LOCATION,
+            "LATITUDE": conflict.LATITUDE,
+            "LONGITUDE": conflict.LONGITUDE,
+            "SOURCE": conflict.SOURCE,
+            "NOTES": conflict.NOTES,
+            "FATALITIES": conflict.FATALITIES
+            } for conflict in conflicts
+    ]
+
+    return jsonify(results)
 
 
 if __name__ == "__main__":
     app.run()
+
+    # request.method == 'GET':
