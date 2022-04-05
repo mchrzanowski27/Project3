19279b48d53432f521fd5208fbd3276cbdf8d5ab
Caroline Kiorpes <cekiorpes@gmail.com>
2022-04-05 19:05:43 -0400

    Took out args in models.py

--- models.py
+++ models.py
@@ -3,18 +3,18 @@ from app import db
 class Ukraine(db.Model):
     __tablename__ = "Ukraine_Conflict"
 
-    EVENT_ID_CNTY = db.Column(db.String(64), primary_key=True)
-    EVENT_ID_NO_CNTY = db.Column(db.Integer(10), primary_key=True)
-    EVENT_DATE = db.Column(db.String(64))
-    EVENT_TYPE = db.Column(db.String(64))
-    SUB_EVENT_TYPE = db.Column(db.String(64))
-    ACTOR1 = db.Column(db.String(64))
-    LOCATION = db.Column(db.String(64))
-    LATITUDE = db.Column(db.Float(10))
-    LONGITUDE = db.Column(db.Float(10))
+    EVENT_ID_CNTY = db.Column(db.String(), primary_key=True)
+    EVENT_ID_NO_CNTY = db.Column(db.Integer(), primary_key=True)
+    EVENT_DATE = db.Column(db.String())
+    EVENT_TYPE = db.Column(db.String())
+    SUB_EVENT_TYPE = db.Column(db.String())
+    ACTOR1 = db.Column(db.String())
+    LOCATION = db.Column(db.String())
+    LATITUDE = db.Column(db.Float())
+    LONGITUDE = db.Column(db.Float())
     SOURCE = db.Column(db.String())
     NOTES = db.Column(db.String())
-    FATALITIES = db.Column(db.Integer(10))
+    FATALITIES = db.Column(db.Integer())
 
     def __repr__(self):
         return '<Ukraine %r>' % (self.EVENT_TYPE)
