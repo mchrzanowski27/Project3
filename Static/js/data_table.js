061246d1e23cb9e27df3b3a65187a0324b8566c0
phoebeburns <37161658+phoebeburns@users.noreply.github.com>
2022-04-04 20:28:50 -0400

    more dropdown fun

--- Static/js/data_table.js
+++ Static/js/data_table.js
@@ -1,21 +1,53 @@
 
 function dateChange(date) {
     // d3.json("https://your_flas_kapp.com/data").then(function (data) {
-    d3.csv("./Static/Project_3_Test_to_SQL.csv", function (data) {
+    d3.csv("../Project_3_Test_to_SQL.csv", function (data) {
 
         let dropdown2 = d3.select("#selDataset2")
-        let dropdown3 = d3.select("#selDataset3")
+        let datad = data.event_type
+        if (date == 'Any' || date == 'All') {
 
-        if (date == 'All' || date == 'Any') {
-            d3.select('#selDataset2').property('value', 'All');
-            d3.select('#selDataset3').property('value', 'All');
-        } else {
-
-            let data1 = Project_3_Test_to_SQL.csv
-            let dd_data = table_data.filter(filterJawn => {filterJawn.event_date == date 
-            })
-            for(dd of dd_data){
+            for (dd of datad) {
                 dropdown2.append("option").text(event_type).property("value", event_type);
+            }
+        } else {
+            let dd_data = datad.filter(filterJawn => {
+                filterJawn.event_date == date
+            })
+            for (dd of dd_data) {
+                dropdown2.append("option").text(event_type).property("value", event_type);
+            }
+        }
+    })
+
+}
+
+function eventChange(event) {
+    // d3.json("https://your_flas_kapp.com/data").then(function (data) {
+    d3.csv("../Project_3_Test_to_SQL.csv", function (data) {
+
+        let date = d3.select("#selDataset1").node().value;
+        let dropdown3 = d3.select("#selDataset3")
+        let datad = data.ACTOR1
+
+        if ((event == 'All' || event == 'Any') && (date == 'All' || date == 'Any')) {
+            for (dd of datad) {
+                dropdown3.append("option").text(actor1).property("value", actor1);
+            }
+
+        } else if ((event == 'All' || event == 'Any') && date != 'All' && date != 'Any') {
+
+            let dd_data = data1.filter(filterJawn => {
+                filterJawn.event_date == date
+            })
+            for (dd of dd_data) {
+                dropdown3.append("option").text(actor).property("value", actor);
+            }
+        } else {
+            let dd_data = data1.filter(filterJawn => {
+                filterJawn.event_date == date && filterJawn.EVENT_TYPE
+            })
+            for (dd of dd_data) {
                 dropdown3.append("option").text(actor).property("value", actor);
             }
         }
@@ -83,42 +115,25 @@ function init() {
     let dropdown3 = d3.select("#selDataset3")
 
     //read in the data for dropdown 1
-    d3.csv("./Static/Project_3_Test_to_SQL.csv", function (data) {
+    d3.csv("../Static/Project_3_Test_to_SQL.csv", function (data) {
         // d3.json("https://your_flas_kapp.com/data").then(function (data) {
 
-        let dates = data.EVENT_DATE;
-        for (date of dates) {
+        let event_dates = data.event_date;
+        console.log(event_dates)
+        for (event_date of event_dates) {
             dropdown1.append("option").text(date).property("value", date);
         }
-        dropdown1.append("option").text("All").property("All")
-        dropdown2.append("option").text("Any").property("Any")
+        dropdown1.append("option").text("All").property("value", "All")
+        dropdown1.append("option").text("Any").property("value", "Any")
+        dropdown1.append("option").text("--").property("value", "")
+        dropdown2.append("option").text("--").property("value", "")
+        dropdown3.append("option").text("--").property("value", "")
 
     })
-    //read in the data for dropdown 2
-    // d3.json("https://your_flas_kapp.com/data").then(function (data) {
-    d3.csv("./Static/Project_3_Test_to_SQL.csv", function (data) {
 
-        let event_types = data.EVENT_TYPE;
-        for (event_type of event_types) {
-            dropdown2.append("option").text(event_type).property("value", event_type);
-        }
-        dropdown2.append("option").text("All").property("All")
-        dropdown2.append("option").text("Any").property("Any")
-    })
-
-    //read in the data for dropdown 3
-    // d3.json("https://your_flas_kapp.com/data").then(function (data) {
-    d3.csv("./Static/Project_3_Test_to_SQL.csv", function (data) {
-        let actors = data.ACTOR1;
-        for (actor of actors) {
-            dropdown3.append("option").text(actor).property("value", actor);
-        }
-        dropdown3.append("option").text("All").property("All")
-        dropdown2.append("option").text("Any").property("Any")
-    })
-    d3.select('#selDataset1').property('value', 'All');
-    d3.select('#selDataset2').property('value', 'All');
-    d3.select('#selDataset3').property('value', 'All');
+    d3.select('#selDataset1').property('value', '');
+    d3.select('#selDataset2').property('value', '');
+    d3.select('#selDataset3').property('value', '');
 }
 
 init()
