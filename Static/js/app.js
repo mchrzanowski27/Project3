88f3c064eb6ecaa56c0bc74b02e9488ce95577ef
Caroline Kiorpes <cekiorpes@gmail.com>
2022-04-07 14:59:40 -0400

    Working on charts

--- Static/js/app.js
+++ Static/js/app.js
@@ -7,35 +7,97 @@ d3.json("/api/data").then(function(data){
     // for (row of data)
     //     console.log(row.ACTOR1);
 
+    let actor = "Military Forces of Russia (2000-)";
+    // let actor = "Military Forces of Ukraine (2019-)";
+    let type = "Explosions";
+    
+    let filteredActor = data.filter(row => row.ACTOR1 === actor);
+    console.log(filteredActor);
+
+    let filteredEvent = filteredActor.filter(row => row.EVENT_TYPE === type);
+    console.log(filteredEvent);
+
+    let fatalities = filteredEvent.map(row=> row.FATALITIES);
+    console.log(fatalities);
+
+    let sub_type = filteredEvent.map(row=> row.SUB_EVENT_TYPE);
+    console.log(sub_type);
+
+    // let sum = filteredEvent.reduce((sum, current)=>sum + current.FATALITIES, 0);
+    // console.log(sum);
+
     //Bar graph of fatalities by actor
     let trace1 = [
         {
-            x: data.map(row=> row.ACTOR1),
-            y: data.map(row=> row.FATALITIES),
-            type: "bar"
+            x: sub_type,
+            y: fatalities,
+            type: "bar",
+            hoverinfo:"skip",
         }
     ];
-
+        
     let layout = {
-        title: "Fatalities by Actor"
+        title: "Fatalities by Actor",
+        yaxis: {
+            title: "Fatalities",
+        }
     };
 
     Plotly.newPlot("graph1", trace1, layout);
 
-    //Bar graph of sub-events by actor- make stacked bar graph?
+    //Bar graph of sub-events by actor- very similar to above graph
     let trace2=[
         {
-            x: data.map(row=>row.SUB_EVENT_TYPE),
-            y: data.map(row=>row.ACTOR1),
+            x: sub_type,
             type: "bar"
         }
     ];
 
     let layout2 = {
-        title: "Violence by Actor"
+        title: "Violence by Actor",
     };
 
-    Plotly.newPlot("graph2", trace2, layout2);
+    // Plotly.newPlot("graph2", trace2, layout2);
+
+    // let filteredDate = filteredActor.filter(row=>row.EVENT_DATE === row.EVENT_DATE);
+    // console.log(filteredDate);
+
+    let trace3 = [
+        {
+            // x: filteredActor.reduce((sum, current)=>sum + current.LOCATION, 0),
+            // x: filteredActor.filter(row=> row.EVENT_DATE).length,
+            x: filteredActor.map(row=> row.EVENT_DATE),
+            y: filteredActor.map(row=> row.FATALITIES),
+            type: "bar",
+        }
+    ];
+
+    let layout3 = {
+        title: "Fatalities by Actor by Date",
+        yaxis: {
+            title: "Fatalities",
+        }
+    };
+
+    Plotly.newPlot("graph2", trace3, layout3);
+
+    //Bubble chart of sub event type by actor?
+    let bubble = [
+        {
+            x: filteredActor.map(row => row.SUB_EVENT_TYPE),
+            text:filteredActor.map(row => row.SUB_EVENT_TYPE),
+            mode: "markers",
+        //     marker: {
+        //         size: filteredActor.map(row=>row.FATALITIES),
+        //     }
+        }
+    ];
+
+    let bubble_layout = {
+        title: "Violence by Actor",
+    };
+
+    // Plotly.newPlot("graph2", bubble, bubble_layout);
 });
 
 //JS file - Phoebe messing with dropdown ideas
