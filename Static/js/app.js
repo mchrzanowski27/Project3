955c50ae00626d50ac5bb8225ba986bb5b771a99
phoebeburns <37161658+phoebeburns@users.noreply.github.com>
2022-04-10 13:09:21 -0400

    final(hopefully)

--- Static/js/app.js
+++ Static/js/app.js
@@ -2,28 +2,43 @@
 
 function eventChange(event) {
     d3.json("/api/data").then(function (data) {
-
+        
         let event = d3.select("#selDataset1").node().value;
+        d3.selectAll("#selDataset2 > *").remove()
         let dropdown2 = d3.select("#selDataset2")
         let actors1 = data.map(i => i.ACTOR1)
+
         uniqueSet2 = new Set(actors1)
         actors = Array.from(uniqueSet2)
         dropdown2.append("option").text("All").property("value", "All")
         actors.sort()
+
 
         if (event == 'All' || event == 'Any') {
             for (actor of actors) {
                 dropdown2.append("option").text(actor).property("value", actor);
             }
         } else  {
-            actors = data.filter(filterJawn => {
-                filterJawn.event_type == event
-            }).map( i=> i.ACTOR1)
-            for (actor of actors) {
+            data2 = data.filter((filterJawn) =>  filterJawn.event_type = event)
+            actors2 = data2.map( i => i.ACTOR1)
+            uniqueSet3 = new Set(actors2)
+            actors3 = Array.from(uniqueSet3)
+            
+            actors3.sort()
+            for (actor of actors3) {
                 dropdown2.append("option").text(actor).property("value", actor);
             }
         }         
     })
+
+}
+
+function actorChange(actor){
+
+    let event = d3.select("#selDataset1").node().value;
+    let actor = d3.select("#selDataset2").node().value;
+
+    charts(actor,event)
 
 }
 
@@ -40,7 +55,7 @@ function init() {
         let event_types1 = data.map( i => i.EVENT_TYPE );
         uniqueSet = new Set(event_types1)
         event_types = Array.from(uniqueSet)
-        // event_types.sort((a, b) => a.EVENT_TYPE.localCompare(b.EVENT_TYPE))
+
         console.log(event_types)
         event_types.sort()
         dropdown1.append("option").text("All").property("value", "All")
@@ -49,9 +64,6 @@ function init() {
             // if(!dropdown1.option.value(event_type))
                 dropdown1.append("option").text(event_type).property("value", event_type);
         }
-
-        // dropdown2.append("option").text("--").property("value", "")
-        // dropdown3.append("option").text("--").property("value", "")
 
     })
 
